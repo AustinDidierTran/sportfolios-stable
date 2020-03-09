d86009a58ddfe5f14bb187bb3d9c8b601aa645bf
Austin-Didier Tran <tran.austin.didier@gmail.com>
2020-03-09 15:16:18 -0400

    Adjustments for staging

--- api/src/db/tmp/user_delete.sql
+++
@@ -1,4 +0,0 @@
-CREATE RULE user_delete AS ON DELETE TO users
-DO INSTEAD
-(UPDATE users SET deleted_at = now() WHERE users.id = old.id;
-UPDATE user_to_team SET deleted_at = now() WHERE user_to_team.user_id = old.id;)
