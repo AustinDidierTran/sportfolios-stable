8342fc12c63fdbe83e488836b5c22708ef70b154
Austin-Didier Tran <tran.austin.didier@gmail.com>
2020-04-04 20:07:57 -0400

    Users List is now visible in admin panel

---
+++ app/src/components/MUI/TableHead/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import TableHead from '@material-ui/core/TableHead';
+
+export default function CustomTableHead(props) {
+  return (
+    <TableHead {...props} >{props.children}</TableHead>
+  );
+}
--- app/src/views/AdminPanel/UsersTable/index.jsx
+++ app/src/views/AdminPanel/UsersTable/index.jsx
@@ -1,24 +1,53 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
-import { Card, CardContent, Typography } from '../../../components/MUI';
+import { Card, CardContent, Table, TableBody, TableCell, TableHead, TableRow, Typography } from '../../../components/MUI';
 import styles from './UsersTable.module.css';
 import api from '../../../actions/api';
-import { Store } from '../../../Store';
 
 export default function UsersTable() {
   const { t } = useTranslation();
-  const { state: { authToken } } = useContext(Store);
+  const [users, setUsers] = useState([]);
+
+  const updateUsers = async () => {
+    const res = await api('/api/admin/users');
+
+    console.log('res', res);
+
+    setUsers(res.data);
+  }
 
   useEffect(() => {
-    console.log('inside useeffect')
-    api('/api/admin/users', { authToken }).then(data => console.log('data', data));
+    updateUsers()
   }, []);
 
   return (
     <Card className={styles.card}>
       <CardContent className={styles.inputs}>
         <Typography gutterBottom variant="h5" component="h2">{t('users_table_title')}</Typography>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>User ID</TableCell>
+              <TableCell>First Name</TableCell>
+              <TableCell>Last Name</TableCell>
+              <TableCell>Emails</TableCell>
+              <TableCell>Is App Admin</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {users.map((user) => (
+              <TableRow key={user.id}>
+                <TableCell>{user.id}</TableCell>
+                <TableCell>{user.first_name}</TableCell>
+                <TableCell>{user.last_name}</TableCell>
+                <TableCell>{user.emails.join(', ')}</TableCell>
+                <TableCell>{user.app_role}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+
       </CardContent>
     </Card>
   )
