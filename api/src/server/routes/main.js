90d688d6d465bd9b85c2a326a4d3cf991f18ad50
julienbernat <57548160+julienbernat@users.noreply.github.com>
2020-05-07 11:12:02 -0400

    Update api/src/server/routes/main.js
    Co-authored-by: Austin-Didier Tran <tran.austin.didier@gmail.com>

--- api/src/server/routes/main.js
+++ api/src/server/routes/main.js
@@ -4,7 +4,6 @@ const queries = require('../../db/queries/main');
 const router = new Router();
 const BASE_URL = '/api/data/main';
 
-console.log('Configuring all routes');
 
 router.get(`${BASE_URL}/all`, async ctx => {
   try {
