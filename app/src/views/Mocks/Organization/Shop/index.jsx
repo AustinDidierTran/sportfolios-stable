92d0b6e2bbba7f5fb6b9db0f742997c60f8ded0c
julienbernat <57548160+julienbernat@users.noreply.github.com>
2020-05-18 15:19:12 -0400

    Ajout de la selection entre general et shop

--- app/src/views/Mocks/Organization/Shop/index.jsx
+++ app/src/views/Mocks/Organization/Shop/index.jsx
@@ -1,25 +1,5 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
-
-import styles from './Shop.module.css';
-import ButtonGroup from '@material-ui/core/ButtonGroup';
-
-import { Avatar } from '../../../../components/Custom';
-import {
-  Button,
-  Typography,
-  Container,
-} from '../../../../components/MUI';
 
 export default function BasicInfos(props) {
-  return (
-    <ButtonGroup
-      variant="contained"
-      color="primary"
-      className={styles.buttons}
-    >
-      <Button>Inscription</Button>
-      <Button>Shop</Button>
-    </ButtonGroup>
-  );
+  return <h3>Vous pouvez tout achetez c'est gratuit :)</h3>;
 }
