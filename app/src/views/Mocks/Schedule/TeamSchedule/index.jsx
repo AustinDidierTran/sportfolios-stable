3180143d328cbe8f0eacf6a72128f9b6bc0b140f
julienbernat <57548160+julienbernat@users.noreply.github.com>
2020-05-19 15:33:11 -0400

    Ajout des parties

--- app/src/views/Mocks/Schedule/TeamSchedule/index.jsx
+++ app/src/views/Mocks/Schedule/TeamSchedule/index.jsx
@@ -7,9 +7,8 @@ import FormControl from '@material-ui/core/FormControl';
 
 import {
   Card,
-  Container,
   Typography,
-  Button,
+  Container,
 } from '../../../../components/MUI';
 
 import styles from './TeamSchedule.module.css';
@@ -19,18 +18,103 @@ export default function TeamSchedule(props) {
   const { t } = useTranslation();
 
   return (
-    <Card className={styles.card}>
-      <FormControl className={styles.select}>
-        <InputLabel>{t('team')}</InputLabel>
-        <Select>
-          <MenuItem value="" disabled>
-            {t('team')}
-          </MenuItem>
-          <MenuItem value={'Manic'}>Manic</MenuItem>
-          <MenuItem value={'SGC'}>SGC</MenuItem>
-          <MenuItem value={'Quake'}>Quake</MenuItem>
-        </Select>
-      </FormControl>
-    </Card>
+    <Container className={styles.container}>
+      <Card className={styles.card}>
+        <FormControl className={styles.select}>
+          <InputLabel>{t('team')}</InputLabel>
+          <Select>
+            <MenuItem value="" disabled>
+              {t('team')}
+            </MenuItem>
+            <MenuItem value={'Manic'}>Manic</MenuItem>
+            <MenuItem value={'SGC'}>SGC</MenuItem>
+            <MenuItem value={'Quake'}>Quake</MenuItem>
+          </Select>
+        </FormControl>
+      </Card>
+      <Card className={styles.game}>
+        <div className={styles.infos}>
+          <Typography
+            className={styles.time}
+            variant="h6"
+            color="textSecondary"
+          >
+            8:35 - 9:45
+          </Typography>
+          <Typography
+            className={styles.type}
+            variant="h6"
+            color="textSecondary"
+          >
+            Pool Play
+          </Typography>
+          <Typography
+            className={styles.field}
+            variant="h6"
+            color="textSecondary"
+          >
+            Field 4
+          </Typography>
+        </div>
+        <hr />
+        <div className={styles.scores}>
+          <Typography className={styles.leftTeamScore} variant="h5">
+            8
+          </Typography>
+          <Typography className={styles.rightTeamScore} variant="h5">
+            11
+          </Typography>
+          <Typography className={styles.matchup} variant="h3">
+            Manic vs SGC
+          </Typography>
+        </div>
+      </Card>
+      <Card className={styles.game}>
+        <Typography
+          className={styles.matchup}
+          variant="h6"
+          color="secondary"
+        >
+          BAIL
+        </Typography>
+      </Card>
+      <Card className={styles.game}>
+        <div className={styles.infos}>
+          <Typography
+            className={styles.time}
+            variant="h6"
+            color="textSecondary"
+          >
+            10:50 - 12:00
+          </Typography>
+          <Typography
+            className={styles.type}
+            variant="h6"
+            color="textSecondary"
+          >
+            Bracket 1-8
+          </Typography>
+          <Typography
+            className={styles.field}
+            variant="h6"
+            color="textSecondary"
+          >
+            Field 6
+          </Typography>
+        </div>
+        <hr />
+        <div className={styles.scores}>
+          <Typography className={styles.leftTeamScore} variant="h5">
+            13
+          </Typography>
+          <Typography className={styles.rightTeamScore} variant="h5">
+            9
+          </Typography>
+          <Typography className={styles.matchup} variant="h3">
+            Manic vs Quake
+          </Typography>
+        </div>
+      </Card>
+    </Container>
   );
 }
