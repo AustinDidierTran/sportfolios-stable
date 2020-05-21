4e2d7350f1d64839d93212243f1d2ce0aca9d973
julienbernat <57548160+julienbernat@users.noreply.github.com>
2020-05-21 09:16:34 -0400

    Ajout des infos generales

--- app/src/views/Mocks/Event/Infos/index.jsx
+++ app/src/views/Mocks/Event/Infos/index.jsx
@@ -13,33 +13,75 @@ import { useTranslation } from 'react-i18next';
 export default function Infos(props) {
   const { t } = useTranslation();
 
+  const infos = {
+    description:
+      "Ce tournoi est le premier tournoi du circuit CQU7 de l'année",
+    responsable: 'Austin-Didier Tran',
+    email: 'didier@sportfolios.app',
+    phone: '819-123-4567',
+    informations: 'La tente de nourriture est situ',
+    lieu: 'Parc Olympic',
+    adress: '3791, Chemin Queen Mary, Montréal, QC H3V 1A8',
+    sections: [
+      {
+        title: 'Prix',
+        texte: 'Le prix du tournoi est de 280$ par équipe',
+      },
+      {
+        title: 'Alignement recommandé',
+        texte: '21 joueurs par équipe',
+      },
+      {
+        title: 'Règlement du tournoi',
+        texte:
+          'Le tournoi utiliseras la 11eme édition des règlements de la WFDF',
+      },
+    ],
+  };
+
   return (
-    <Card>
-      <Container className={styles.responsable}>
-        <Typography variant="h3">Description</Typography>
+    <Card className={styles.card}>
+      <Container className={styles.lieu}>
+        <Typography variant="h3" color="primary">
+          Lieu
+        </Typography>
+        <Typography variant="h5">{infos.lieu}</Typography>
         <Typography variant="h5" color="textSecondary">
-          Ce tournoi est le premier tournoi du circuit CQU7 de l'année
+          {infos.adress}
+        </Typography>
+      </Container>
+      <Container className={styles.description}>
+        <Typography variant="h3" color="primary">
+          Description
+        </Typography>
+        <Typography variant="h5" color="textSecondary">
+          {infos.description}
         </Typography>
       </Container>
       <Container className={styles.responsable}>
-        <Typography variant="h3">
+        <Typography variant="h3" color="primary">
           Responsable de l'événement
         </Typography>
+        <Typography variant="h5">{infos.responsable}</Typography>
         <Typography variant="h5" color="textSecondary">
-          Austin-Didier Tran
+          {infos.email}
+          <br />
+          {infos.phone}
         </Typography>
       </Container>
       <Container className={styles.infos}>
-        <Typography variant="h3">Informations Générales</Typography>
-        <Typography variant="h5" color="textSecondary">
-          Bouffe...
+        <Typography variant="h3" color="primary">
+          Informations Générales
         </Typography>
-      </Container>
-      <Container className={styles.infos}>
-        <Typography variant="h3">Lieu</Typography>
-        <Typography variant="h5" color="textSecondary">
-          3791, Chemin Queen Mary, Montréal, QC H3V 1A8
-        </Typography>
+        {infos.sections.map(section => (
+          <Container>
+            <Typography variant="h5">{section.title}:</Typography>
+            <Typography variant="h5" color="textSecondary">
+              {section.texte}
+            </Typography>
+            <hr />
+          </Container>
+        ))}
       </Container>
     </Card>
   );
