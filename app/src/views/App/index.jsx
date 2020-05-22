099311887587d897f5c1613b0f4125bc20d37f59
julienbernat <57548160+julienbernat@users.noreply.github.com>
2020-05-22 13:49:25 -0400

    Update app/src/views/App/index.jsx
    Co-authored-by: Austin-Didier Tran <tran.austin.didier@gmail.com>

--- app/src/views/App/index.jsx
+++ app/src/views/App/index.jsx
@@ -158,7 +158,6 @@ export default function App() {
                 component={ConfirmationEmailSent}
               />
               <Route
-                exact
                 path={ROUTES.team}
                 component={OtherTeam}
               />
