813ef2c1d7fd83fca518918bdcf1d60afbb09528
julienbernat <57548160+julienbernat@users.noreply.github.com>
2020-06-10 13:30:37 -0400

    Resolved problem

---
+++ app/src/views/Organization/Settings/ManageRoles/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Paper } from '../../../components/Custom';
+import { Typography } from '../../../components/MUI';
+import styles from './ManageRoles';
+
+export default function ManageRoles(props) {
+  return <Paper></Paper>;
+}
