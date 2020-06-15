3da867248b16b947f8812b5a743d5e261e4b93f5
Austin-Didier Tran <tran.austin.didier@gmail.com>
2020-06-15 08:09:06 -0400

    Added missing primary keys and unique constraints

---
+++ api/src/db/migrations/sqls/20200615115905-add-missing-primary-keys-down.sql
@@ -0,0 +1,13 @@
+/* Replace with your SQL commands */
+ALTER TABLE phase_link
+  DROP CONSTRAINT phase_link_ufinal,
+  DROP CONSTRAINT phase_link_pkey;
+
+ALTER TABLE game_teams
+  DROP CONSTRAINT game_teams_pkey;
+
+ALTER TABLE entities_role
+  DROP CONSTRAINT entities_role_pkey;
+
+ALTER TABLE entities_photo
+  DROP CONSTRAINT entities_photo_pkey;
---
+++ api/src/db/migrations/sqls/20200615115905-add-missing-primary-keys-up.sql
@@ -0,0 +1,13 @@
+/* Replace with your SQL commands */
+ALTER TABLE entities_photo
+  ADD CONSTRAINT entities_photo_pkey PRIMARY KEY (entity_id);
+
+ALTER TABLE entities_role
+  ADD CONSTRAINT entities_role_pkey PRIMARY KEY (entity_id, entity_id_admin);
+
+ALTER TABLE game_teams
+  ADD CONSTRAINT game_teams_pkey PRIMARY KEY (game_id);
+
+ALTER TABLE phase_link
+  ADD CONSTRAINT phase_link_pkey PRIMARY KEY (initial_phase, initial_position),
+  ADD CONSTRAINT phase_link_ufinal UNIQUE (final_phase, final_position);
