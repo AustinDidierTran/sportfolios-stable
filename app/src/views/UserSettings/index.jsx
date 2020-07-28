5c1e1e6b1bba09da8246990f70d69081227c4f1e
Austin-Didier Tran <tran.austin.didier@gmail.com>
2020-07-28 12:31:21 -0400

    Added missing translations

--- app/src/views/UserSettings/index.jsx
+++ app/src/views/UserSettings/index.jsx
@@ -8,10 +8,12 @@ import Disconnect from './Disconnect';
 import Email from './Email';
 import { Container } from '../../components/Custom';
 import { formatPageTitle } from '../../utils/stringFormats';
+import { useTranslation } from 'react-i18next';
 
 export default function UserSettings() {
+  const { t } = useTranslation();
   useEffect(() => {
-    document.title = formatPageTitle('Settings');
+    document.title = formatPageTitle(t('settings'));
   }, []);
 
   return (
