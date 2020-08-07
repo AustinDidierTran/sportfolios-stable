fe3aa18f8f06b9416a66ee292546ea4eb98a4b9d
julienbernat <57548160+julienbernat@users.noreply.github.com>
2020-08-07 13:08:14 -0400

    You can now change pool name and see w l + - +/-

--- app/src/views/ScheduleManager/index.jsx
+++ app/src/views/ScheduleManager/index.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Paper } from '../../components/Custom';
+import { TextField } from '../../components/MUI';
 import { useTranslation } from 'react-i18next';
-import { Typography } from '@material-ui/core';
 import AddTeams from './AddTeams';
 import Games from './Games';
 import Ranking from './Ranking';
@@ -11,10 +11,16 @@ import { GLOBAL_ENUM } from '../../../../common/enums';
 import { useQuery } from '../../hooks/queries';
 import uuid from 'uuid';
 import { updateRanking } from './RankingFunctions';
+import { formatPageTitle } from '../../utils/stringFormats';
+import { useFormInput } from '../../hooks/forms';
 
 export default function ScheduleManager() {
   const { t } = useTranslation();
-  const { teams, games, ranking } = useQuery();
+  const { teams, games, ranking, title } = useQuery();
+
+  useEffect(() => {
+    document.title = formatPageTitle(JSON.parse(title));
+  }, [title]);
 
   const onDelete = id => {
     setTempGames(oldGames =>
@@ -29,10 +35,12 @@ export default function ScheduleManager() {
 
   const save = () => {
     setTempRanking(updateRanking(tempTeams, tempGames));
+    document.title = formatPageTitle(tempTitle.value);
     goTo(ROUTES.scheduleManager, null, {
       teams: JSON.stringify(tempTeams),
       games: JSON.stringify(tempGames),
       ranking: JSON.stringify(tempRanking),
+      title: JSON.stringify(tempTitle.value),
     });
   };
 
@@ -52,16 +60,24 @@ export default function ScheduleManager() {
     }
     return [];
   };
+
   const getRanking = () => {
     if (ranking) {
       return JSON.parse(ranking);
     }
     return [];
   };
+  const getTitle = () => {
+    if (title) {
+      return JSON.parse(title);
+    }
+    return 'Pool Play';
+  };
 
   const [tempGames, setTempGames] = useState(getGames());
   const [tempTeams, setTempTeams] = useState(getTeams());
   const [tempRanking, setTempRanking] = useState(getRanking());
+  const tempTitle = useFormInput(getTitle());
 
   const addTeam = (e, team) => {
     const id = team.id || uuid.v1();
@@ -102,13 +118,11 @@ export default function ScheduleManager() {
 
   return (
     <Paper className={styles.main}>
-      <Typography
-        variant="h4"
-        component="p"
-        style={{ marginBottom: '8px' }}
-      >
-        {t('Welcome to the tournament')}
-      </Typography>
+      <TextField
+        className={styles.title}
+        size="sm"
+        {...tempTitle.inputProps}
+      />
       <Container className={styles.container}>
         <AddTeams
           className={styles.teams}
