a9af7b61bf754ec980d23af17bff8d3b4a4bf92d
Lafleur <you@example.com>
2020-08-10 14:12:15 -0400

    fix

--- app/src/index.jsx
+++ app/src/index.jsx
@@ -15,9 +15,11 @@ function main() {
       navigator.serviceWorker
         .register('/service-worker.js')
         .then(registration => {
+          /* eslint-disable-next-line */
           console.log('SW registered: ', registration);
         })
         .catch(registrationError => {
+          /* eslint-disable-next-line */
           console.log('SW registration failed: ', registrationError);
         });
     });
