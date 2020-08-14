5b7e6ad09b0a091e519f549cd7a1c5524558e397
Lafleur <you@example.com>
2020-08-14 10:57:49 -0400

    Adding a doc to the readMe, plus small changes

--- app/src/views/Main/General/UpcomingEvents/index.jsx
+++ app/src/views/Main/General/UpcomingEvents/index.jsx
@@ -1,11 +1,10 @@
-import { useState, useEffect } from 'react';
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import clsx from 'clsx';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 
-import { CardContent } from '@material-ui/core/CardContent/index.js';
+import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
 import Collapse from '@material-ui/core/Collapse';
 import IconButton from '@material-ui/core/IconButton';
