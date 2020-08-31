d8e55600d2f4fe8619061a73b6ce0eed6ca0f621
julienbernat <57548160+julienbernat@users.noreply.github.com>
2020-08-31 15:22:22 -0400

    Added dialog can open and close dialog but thats it

--- app/src/tabs/Schedule/index.jsx
+++ app/src/tabs/Schedule/index.jsx
@@ -1,5 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { ENTITIES_ROLE_ENUM } from '../../../../common/enums';
+import { Button } from '../../components/Custom';
+import styles from './Schedule.module.css';
+import { useTranslation } from 'react-i18next';
+import AddGame from './AddGame';
 
-export default function ScheduleTab() {
+export default function ScheduleTab(props) {
+  const { t } = useTranslation();
+  const {
+    basicInfos: { role },
+  } = props;
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openDialog = () => {
+    setIsOpen(true);
+  };
+
+  if (role === ENTITIES_ROLE_ENUM.ADMIN) {
+    return (
+      <>
+        <Button
+          size="small"
+          variant="contained"
+          endIcon="Add"
+          style={{ margin: '8px' }}
+          onClick={openDialog}
+          className={styles.button}
+        >
+          {t('add_game')}
+        </Button>
+        <AddGame isOpen={isOpen} />
+      </>
+    );
+  }
   return <h1>SCHEDULE</h1>;
 }
