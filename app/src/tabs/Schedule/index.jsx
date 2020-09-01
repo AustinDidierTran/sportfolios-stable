0f1b8de344a240e35f8a852d2438b91f27788478
julienbernat <57548160+julienbernat@users.noreply.github.com>
2020-09-01 13:27:06 -0400

    WIP

--- app/src/tabs/Schedule/index.jsx
+++ app/src/tabs/Schedule/index.jsx
@@ -12,14 +12,29 @@ export default function ScheduleTab(props) {
   } = props;
 
   const [isOpen, setIsOpen] = useState(false);
+  const [phase, setPhase] = useState(false);
 
   const openDialog = () => {
     setIsOpen(true);
   };
 
+  const openPhase = () => {
+    setPhase(true);
+  };
+
   if (role === ENTITIES_ROLE_ENUM.ADMIN) {
     return (
       <>
+        <Button
+          size="small"
+          variant="contained"
+          endIcon="Add"
+          style={{ margin: '8px' }}
+          onClick={openPhase}
+          className={styles.button}
+        >
+          {t('add_phase')}
+        </Button>
         <Button
           size="small"
           variant="contained"
