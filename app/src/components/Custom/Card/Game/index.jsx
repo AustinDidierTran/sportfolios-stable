a821eab6c0962fa6958db00352ca12dcac3bd5cd
julienbernat <57548160+julienbernat@users.noreply.github.com>
2020-09-04 11:42:29 -0400

    Improved games ui

--- app/src/components/Custom/Card/Game/index.jsx
+++ app/src/components/Custom/Card/Game/index.jsx
@@ -5,28 +5,40 @@ import styles from './GameItem.module.css';
 import { Typography, Card } from '../../../MUI';
 import { formatDate } from '../../../../utils/stringFormats';
 import moment from 'moment';
+import { ListItemText } from '@material-ui/core';
 
 export default function GameItem(props) {
   const { teams, field, start_time: startTime, phaseName } = props;
+
+  const team1 = teams[0];
+  const team2 = teams[1];
+
   return (
     <Card className={styles.game}>
       <div className={styles.main}>
-        <Typography className={styles.field}>{field}</Typography>
-        <Typography className={styles.phase}>{phaseName}</Typography>
-        <Typography className={styles.time}>
-          {formatDate(moment(startTime), 'h:mm ddd')}
+        <Typography className={styles.phase} color="textSecondary">
+          {phaseName}
+        </Typography>
+        <ListItemText
+          className={styles.name}
+          primary={formatDate(moment(startTime), 'h:mm')}
+          secondary={formatDate(moment(startTime), 'ddd')}
+        ></ListItemText>
+        <Typography className={styles.field} color="textSecondary">
+          {field}
         </Typography>
       </div>
-      {teams.map(team => (
-        <div className={styles.team}>
-          <Typography className={styles.name} variant="h6">
-            {team.name}
-          </Typography>
-          <Typography className={styles.score} variant="h6">
-            {team.score}
-          </Typography>
-        </div>
-      ))}
+      <div className={styles.teams}>
+        <Typography className={styles.name1}>{team1.name}</Typography>
+        <Typography className={styles.score1}>
+          {team1.score}
+        </Typography>
+        <Typography className={styles.union}>-</Typography>
+        <Typography className={styles.name2}>{team2.name}</Typography>
+        <Typography className={styles.score2}>
+          {team2.score}
+        </Typography>
+      </div>
     </Card>
   );
 }
