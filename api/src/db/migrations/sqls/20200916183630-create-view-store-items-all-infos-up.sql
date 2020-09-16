d23635c99e6644102d4036f9d55818ba863e83ae
Victor <vicfreve@gmail.com>
2020-09-16 14:47:46 -0400

    Added views that contain all infos for entities, events and shop items so it can be reused easily

---
+++ api/src/db/migrations/sqls/20200916183630-create-view-store-items-all-infos-up.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE VIEW store_items_all_infos AS
+SELECT entity_id,
+price.stripe_price_id,
+product.stripe_product_id,
+label,
+description,
+amount,
+price.active,
+photo_url
+FROM store_items items
+LEFT JOIN stripe_price price ON items.stripe_price_id=price.stripe_price_id
+LEFT JOIN stripe_product product ON product.stripe_product_id = price.stripe_product_id
+LEFT JOIN entities e on items.entity_id=e.id
+;
