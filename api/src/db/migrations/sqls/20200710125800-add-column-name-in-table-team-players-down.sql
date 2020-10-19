193a49e965c145388f466564a9bb9a4ff5866316
julienbernat <57548160+julienbernat@users.noreply.github.com>
2020-10-19 09:45:50 -0400

    Quick fix db-migrate reset

--- api/src/db/migrations/sqls/20200710125800-add-column-name-in-table-team-players-down.sql
+++ api/src/db/migrations/sqls/20200710125800-add-column-name-in-table-team-players-down.sql
@@ -7,6 +7,19 @@ DROP COLUMN id;
 DELETE FROM team_players 
 WHERE person_id IS NULL;
 
+DELETE FROM team_players
+  WHERE person_id IN
+    (SELECT person_id FROM 
+        (SELECT person_id,
+         ROW_NUMBER() OVER( PARTITION BY person_id,
+         roster_id
+        ORDER BY  person_id ) AS row_num
+        FROM team_players ) t
+        WHERE t.row_num > 1 );
+        
 ALTER TABLE team_players
 ALTER COLUMN person_id SET NOT NULL,
 ADD PRIMARY KEY (roster_id,person_id);
+
+
+
