978634043c1da40a42d498f59b66c14ccf9c8a4e
Victor <vicfreve@gmail.com>
2020-11-05 16:48:29 -0500

    Backend queries

--- api/src/db/helpers/notifications.js
+++ api/src/db/helpers/notifications.js
@@ -1,57 +1,49 @@
 const knex = require('../connection');
 
-const createFollowNotification = async (sender_id, target_id) => {
-  return knex('notification_follow').insert({
-    sender_id,
-    target_id,
-  });
+const addNotification = async infos => {
+  return knex('notifications').insert(infos);
 };
 
-const deleteFollowNotification = async (sender_id, target_id) => {
-  return knex('notification_follow')
-    .where({ sender_id, target_id })
+const deleteNotification = async notification_id => {
+  return knex('notifications')
+    .where({
+      id: notification_id,
+    })
     .del();
 };
 
-const seeFollowNotifications = async (sender_id, target_id) => {
-  return knex('notification_follow')
+const clickNotification = async notification_id => {
+  return knex('notifications')
+    .where({
+      id: notification_id,
+    })
+    .update({ clicked_at: new Date() });
+};
+
+const countUnseenNotifications = async user_id => {
+  return knex('notifications')
+    .select('*')
+    .where({ user_id, seen_at: null })
+    .count('*');
+};
+
+const seeNotifications = async user_id => {
+  return knex('notifications_view')
     .update({ seen_at: new Date() })
-    .where({ sender_id, target_id });
+    .where({ user_id });
 };
 
 const getAllNotifications = async user_id => {
-  const followNotifications = await knex
-    .select(
-      'nf.sender_id',
-      'nf.seen_at',
-      'nf.created_at',
-      'ep.photo_url',
-      'en.name',
-      'en.surname',
-    )
-    .from('notification_follow AS nf')
-    .leftJoin(
-      'entities_name AS en',
-      'nf.sender_id',
-      '=',
-      'en.entity_id',
-    )
-    .leftJoin(
-      'entities_photo AS ep',
-      'nf.sender_id',
-      '=',
-      'ep.entity_id',
-    )
-    .where('nf.target_id', user_id)
-    .orderBy('created_at', 'desc');
-  return [
-    ...followNotifications.map(n => ({ ...n, type: 'follow' })),
-  ];
+  return knex('notifications_view')
+    .select('*')
+    .where({ user_id });
 };
 
 module.exports = {
-  createFollowNotification,
-  deleteFollowNotification,
   getAllNotifications,
-  seeFollowNotifications,
+  seeNotifications,
+  countUnseenNotifications,
+  clickNotification,
+  deleteNotification,
+  addNotification,
 };
--- api/src/server/routes/private/notifications.js
+++ api/src/server/routes/private/notifications.js
@@ -1,27 +1,99 @@
 const Router = require('koa-router');
+const { STATUS_ENUM } = require('../../../../../common/enums');
+const { ERROR_ENUM } = require('../../../../../common/errors');
 const queries = require('../../../db/queries/notifications');
 
 const router = new Router();
 const BASE_URL = '/api/notifications';
 
-router.post(`${BASE_URL}/follow/see`, async ctx => {
-  await queries.seeFollowNotification(
-    ctx.body.userInfo.id,
-    ctx.request.body.follower,
+router.delete(`${BASE_URL}/delete`, async ctx => {
+  const res = await queries.deleteNotification(
+    ctx.request.body.notificationId,
   );
-  ctx.body = {
-    status: 'success',
-  };
+  if (res) {
+    ctx.status = STATUS_ENUM.SUCCESS;
+    ctx.body = {
+      status: STATUS_ENUM.SUCCESS_STRING,
+    };
+  } else {
+    ctx.status = STATUS_ENUM.ERROR;
+    ctx.body = {
+      status: STATUS_ENUM.ERROR_STRING,
+      message: ERROR_ENUM.ERROR_OCCURED,
+    };
+  }
+});
+
+router.put(`${BASE_URL}/click`, async ctx => {
+  const res = await queries.clickNotification(
+    ctx.request.body.notificationId,
+  );
+  if (res) {
+    ctx.status = STATUS_ENUM.SUCCESS;
+    ctx.body = {
+      status: STATUS_ENUM.SUCCESS_STRING,
+    };
+  } else {
+    ctx.status = STATUS_ENUM.ERROR;
+    ctx.body = {
+      status: STATUS_ENUM.ERROR_STRING,
+      message: ERROR_ENUM.ERROR_OCCURED,
+    };
+  }
+});
+
+router.put(`${BASE_URL}/see`, async ctx => {
+  const res = await queries.seeNotifications(ctx.body.userInfo.id);
+  if (res) {
+    ctx.status = STATUS_ENUM.SUCCESS;
+    ctx.body = {
+      status: STATUS_ENUM.SUCCESS_STRING,
+    };
+  } else {
+    ctx.status = STATUS_ENUM.ERROR;
+    ctx.body = {
+      status: STATUS_ENUM.ERROR_STRING,
+      message: ERROR_ENUM.ERROR_OCCURED,
+    };
+  }
+});
+
+router.get(`${BASE_URL}/unseenCount`, async ctx => {
+  const count = await queries.countUnseenNotifications(
+    ctx.body.userInfo.id,
+  );
+  if (count) {
+    ctx.status = STATUS_ENUM.SUCCESS;
+    ctx.body = {
+      status: STATUS_ENUM.SUCCESS_STRING,
+      data: count,
+    };
+  } else {
+    ctx.status = STATUS_ENUM.ERROR;
+    ctx.body = {
+      status: STATUS_ENUM.ERROR_STRING,
+      message: ERROR_ENUM.ERROR_OCCURED,
+    };
+  }
 });
 
 router.get(`${BASE_URL}/all`, async ctx => {
   const notifications = await queries.getAllNotifications(
     ctx.body.userInfo.id,
   );
-  ctx.body = {
-    status: 'success',
-    data: notifications,
-  };
+  if (notifications) {
+    ctx.status = STATUS_ENUM.SUCCESS;
+    ctx.body = {
+      status: STATUS_ENUM.SUCCESS_STRING,
+      data: notifications,
+    };
+  } else {
+    ctx.status = STATUS_ENUM.ERROR;
+    ctx.body = {
+      status: STATUS_ENUM.ERROR_STRING,
+      message: ERROR_ENUM.ERROR_OCCURED,
+    };
+  }
 });
 
 module.exports = router;
