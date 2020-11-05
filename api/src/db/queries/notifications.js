9995b060707a0a872827557be1eaa3a1fd7e4ac9
Victor <vicfreve@gmail.com>
2020-11-05 16:48:29 -0500

    Backend queries

--- api/src/db/queries/notifications.js
+++ api/src/db/queries/notifications.js
@@ -1,10 +1,25 @@
 const {
   getAllNotifications: getAllNotificationsHelper,
-  seeFollowNotifications: seeFollowNotificationHelper,
+  seeNotifications: seeNotificationsHelper,
+  countUnseenNotifications: countUnseenNotificationsHelper,
+  clickNotification: clickNotificationHelper,
+  deleteNotification: deleteNotificationHelper,
 } = require('../helpers/notifications');
 
-const seeFollowNotification = async (user_id, query) => {
-  return seeFollowNotificationHelper(user_id, query);
+const seeNotifications = async user_id => {
+  return seeNotificationsHelper(user_id);
+};
+
+const countUnseenNotifications = async user_id => {
+  return countUnseenNotificationsHelper(user_id);
+};
+
+const clickNotification = async notification_id => {
+  return clickNotificationHelper(notification_id);
+};
+
+const deleteNotification = async notification_id => {
+  return deleteNotificationHelper(notification_id);
 };
 
 const getAllNotifications = async user_id => {
@@ -13,5 +28,8 @@ const getAllNotifications = async user_id => {
 
 module.exports = {
   getAllNotifications,
-  seeFollowNotification,
+  seeNotifications,
+  countUnseenNotifications,
+  clickNotification,
+  deleteNotification,
 };
