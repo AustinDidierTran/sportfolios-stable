bd21cde7e008d27a572c75d6024d4130fed8e7b2
Philippe Garand <philippe.garand@hotmail.com>
2020-11-09 16:36:44 -0500

    Can now save changes in interactive tool. Also changed table game field/start_time to fieldId et timeslotId. Also minor changes in submitScore dialog

--- api/src/db/migrations/sqls/20201106195349-add-ids-to-timeslots-and-fields-up.sql
+++ api/src/db/migrations/sqls/20201106195349-add-ids-to-timeslots-and-fields-up.sql
@@ -9,3 +9,29 @@ ALTER TABLE event_time_slots
   
 ALTER TABLE event_time_slots
   ADD COLUMN id UUID DEFAULT uuid_generate_v4() PRIMARY KEY;
+
+ALTER TABLE games
+  ADD COLUMN timeslot_id UUID,
+  ADD COLUMN field_id UUID,
+  ADD FOREIGN KEY (timeslot_id) REFERENCES event_time_slots(id),
+  ADD FOREIGN KEY (field_id) REFERENCES event_fields(id);
+
+UPDATE games 
+  SET 
+    timeslot_id = (SELECT id FROM event_time_slots 
+      WHERE event_time_slots.event_id = games.event_id 
+      AND event_time_slots.date = games.start_time),
+    field_id = (SELECT id FROM event_fields 
+      WHERE event_fields.event_id = games.event_id 
+      AND event_fields.field = games.field);
+
+ALTER TABLE games
+  DROP COLUMN start_time,
+  DROP COLUMN end_time,
+  DROP field;
+
+ALTER TABLE event_time_slots
+  ADD COLUMN end_time TIMESTAMP DEFAULT NULL;
+
+ALTER TABLE score_suggestion
+  DROP COLUMN start_time;
