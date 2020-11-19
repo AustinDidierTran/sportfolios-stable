4f299485d0d10a7ad70dab2bf46c20f63dd06f96
Philippe Garand <philippe.garand@hotmail.com>
2020-11-19 12:26:31 -0500

    Added new col role in team-players. Also set captains to existing teams

--- api/src/db/migrations/sqls/20201119154207-add-captain-to-rosters-up.sql
+++ api/src/db/migrations/sqls/20201119154207-add-captain-to-rosters-up.sql
@@ -1,4 +1,10 @@
 ALTER TABLE team_players
   ADD COLUMN role VARCHAR(255);
 
---UPDATE team_players SET role = 'captain' WHERE 
+UPDATE team_players 
+  SET role = 'captain'
+  FROM 
+    (SELECT (array_agg(id))[1] as id 
+      FROM (SELECT * FROM team_players ORDER BY created_at ASC) as playersOrdered 
+    GROUP BY roster_id) AS captainsIds
+  WHERE team_players.id = captainsIds.id;
