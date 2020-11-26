a1ec8f7655dc67ccc46aea02c7152dea8324eac4
Victor <vicfreve@gmail.com>
2020-11-26 16:20:33 -0500

    Handle team selection

--- api/src/server/utils/ChatBot/states/ScoreSubmissionStates/gamesAwaitingScoreList.js
+++ api/src/server/utils/ChatBot/states/ScoreSubmissionStates/gamesAwaitingScoreList.js
@@ -9,6 +9,7 @@ const Response = require('../../response');
 const i18n = require('../../../../../i18n.config');
 const {
   getGamesWithAwaitingScore,
+  getGameTeams,
 } = require('../../../../../db/helpers/entity');
 const {
   getUserIdFromMessengerId,
@@ -17,8 +18,34 @@ const {
 class gamesAwaitingScoreList extends State {
   async handleEvent(webhookEvent) {
     let nextState;
-    nextState = BASIC_CHATBOT_STATES.HOME;
-    console.log(this.getPayload(webhookEvent));
+    const requestedGame = this.getPayload(webhookEvent);
+    if (requestedGame) {
+      const { gameId, playerId } = JSON.parse(requestedGame);
+      const teams = await getGameTeams(gameId, playerId);
+      this.context.chatbotInfos.opponentTeams = [];
+      this.context.chatbotInfos.gameId = gameId;
+      this.context.chatbotInfos.playerId = playerId;
+      chatbotInfos.playerId = playerId;
+      let myTeamFound = false;
+      teams.forEach(team => {
+        if (!myTeamFound && team.player_id) {
+          myTeamFound = true;
+          this.context.chatbotInfos.myRosterId = team.roster_id;
+          this.context.chatbotInfos.myTeamName = team.name;
+        } else {
+          this.context.chatbotInfos.opponentTeams.push({
+            rosterId: team.roster_id,
+            teamName: team.name,
+          });
+        }
+        nextState =
+          SCORE_SUBMISSION_CHATBOT_STATES.AWAITING_SCORE_SUBMISSION;
+      });
+    } else if (this.isStartOver(webhookEvent)) {
+      nextState = BASIC_CHATBOT_STATES.HOME;
+    } else {
+      this.sendIDontUnderstand(webhookEvent);
+    }
     if (nextState) {
       this.context.changeState(nextState);
     }
@@ -31,8 +58,8 @@ class gamesAwaitingScoreList extends State {
     const games = await getGamesWithAwaitingScore(userId);
 
     const quickReplies = games.map(game => {
-      const opponentTeamsString = game.opponent_teams_names.concat(
-        i18n.__('and'),
+      const opponentTeamsString = game.opponent_teams_names.join(
+        ' ' + i18n.__('and') + ' ',
       );
       const date = new Date(game.timeslot);
       const dateString =
@@ -41,8 +68,11 @@ class gamesAwaitingScoreList extends State {
         i18n.__(MONTH_NAMES[date.getMonth()]).substring(0, 3) +
         '.';
       return {
-        payload: game.game_id,
-        title: dateString + ', VS ' + opponentTeamsString,
+        payload: JSON.stringify({
+          gameId: game.game_id,
+          playerId: game.player_id,
+        }),
+        title: dateString + ' VS ' + opponentTeamsString,
       };
     });
     return [
