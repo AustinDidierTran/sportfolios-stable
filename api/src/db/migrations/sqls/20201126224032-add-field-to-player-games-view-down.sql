26d47335681a91667b503bd8329b3e1a6d5ef099
Victor <vicfreve@gmail.com>
2020-11-26 18:18:09 -0500

    Fix

--- api/src/db/migrations/sqls/20201126224032-add-field-to-player-games-view-down.sql
+++ api/src/db/migrations/sqls/20201126224032-add-field-to-player-games-view-down.sql
@@ -1,3 +1,6 @@
+DROP VIEW game_players_view;
+
+
 CREATE OR REPLACE VIEW game_players_view AS
 SELECT t4.user_id AS player_owner,
        t5.person_id AS player_id,
@@ -10,25 +13,25 @@ SELECT t4.user_id AS player_owner,
        t5.event_name,
        t5.role AS player_role
 FROM
-  (SELECT user_entity_role.user_id,
-          user_entity_role.entity_id
-   FROM user_entity_role
-   WHERE user_entity_role.role = 1) t4
+        (SELECT user_entity_role.user_id,
+                user_entity_role.entity_id
+         FROM user_entity_role
+         WHERE user_entity_role.role = 1) t4
 JOIN (
-        (SELECT team_players.person_id,
-                team_players.roster_id AS rost_id,
-                team_players.role
-         FROM team_players) t3
+              (SELECT team_players.person_id,
+                      team_players.roster_id AS rost_id,
+                      team_players.role
+               FROM team_players) t3
       JOIN
-        (SELECT games_all_infos.id AS game_id,
-                games_all_infos.event_id,
-                games_all_infos.timeslot,
-                games_all_infos.notified_end,
-                games_all_infos.notified_start,
-                games_all_infos.event_name,
-                t1.roster_id
-         FROM games_all_infos
-         JOIN
-           (SELECT game_teams.roster_id,
-                   game_teams.game_id
-            FROM game_teams) t1 ON games_all_infos.id = t1.game_id) t2 ON t3.rost_id = t2.roster_id) t5 ON t4.entity_id = t5.person_id;
+              (SELECT games_all_infos.id AS game_id,
+                      games_all_infos.event_id,
+                      games_all_infos.timeslot,
+                      games_all_infos.notified_end,
+                      games_all_infos.notified_start,
+                      games_all_infos.event_name,
+                      t1.roster_id
+               FROM games_all_infos
+               JOIN
+                       (SELECT game_teams.roster_id,
+                               game_teams.game_id
+                        FROM game_teams) t1 ON games_all_infos.id = t1.game_id) t2 ON t3.rost_id = t2.roster_id) t5 ON t4.entity_id = t5.person_id;
