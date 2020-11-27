cc80027a323b7a6f47e5417a7fbcb52bbc28d214
Philippe Garand <philippe.garand@hotmail.com>
2020-11-27 12:18:19 -0500

    better search all entities

--- api/src/db/helpers/data.js
+++ api/src/db/helpers/data.js
@@ -1,5 +1,9 @@
 const knex = require('../connection');
 const { GLOBAL_ENUM } = require('../../../../common/enums');
+const {
+  getAllOwnedEntities,
+  isTeamRegisteredInEvent,
+} = require('./entity');
 
 const addQueryToRecentSearches = async (user_id, search_query) => {
   return knex('previous_search_queries')
@@ -229,93 +233,17 @@ const getTeamsFromQuery = async (query, blackList, whiteList) => {
 
 const getMyTeamsFromQuery = async (userId, eventId, query) => {
   const mappingFunction = async e => ({
-    id: e.id,
-    type: e.type,
-    photoUrl: e.photo_url,
-    name: e.complete_name || e.name,
+    ...e,
+    surname: undefined,
+    complete_name: undefined,
     isRegistered: await isTeamRegisteredInEvent(e.id, eventId),
   });
 
-  // getPersons
-  let entityIds = (
-    await knex('user_entity_role')
-      .select('entity_id')
-      .where({
-        user_id: userId,
-      })
-  ).map(person => person.entity_id);
-
-  let count = 0;
-  let newEntityIds = [];
-
-  // get all entities owned by persons and sub persons
-  do {
-    entityIds = [...newEntityIds, ...entityIds];
-    entityIds = entityIds.filter(
-      (id, index) => entityIds.indexOf(id) === index,
-    );
-
-    newEntityIds = (
-      await knex('entities_role')
-        .select('entity_id')
-        .whereIn('entity_id_admin', entityIds)
-    ).map(entity => entity.entity_id);
-
-    count++;
-  } while (
-    newEntityIds.some(id => !entityIds.includes(id)) &&
-    count < 5
+  const entities = await getAllOwnedEntities(
+    GLOBAL_ENUM.TEAM,
+    userId,
+    query,
   );
-
-  const entities = await knex
-    .select(
-      'entities_formatted.name',
-      'entities_formatted.id',
-      'entities_formatted.type',
-      'entities_formatted.photo_url',
-      'entities_formatted.complete_name',
-    )
-    .from(
-      knex
-        .select(
-          'id',
-          'type',
-          'name',
-          'surname',
-          'entities_photo.photo_url',
-          knex.raw(
-            "string_agg(entities_name.name || ' ' || entities_name.surname, ' ') AS complete_name",
-          ),
-        )
-        .from('entities')
-        .leftJoin(
-          'entities_photo',
-          'entities.id',
-          '=',
-          'entities_photo.entity_id',
-        )
-        .leftJoin(
-          'entities_name',
-          'entities.id',
-          '=',
-          'entities_name.entity_id',
-        )
-        .whereNull('entities.deleted_at')
-        .whereIn('entities.id', newEntityIds)
-        .andWhere('entities.type', GLOBAL_ENUM.TEAM)
-        .groupBy(
-          'id',
-          'type',
-          'name',
-          'surname',
-          'entities_photo.photo_url',
-        )
-        .as('entities_formatted'),
-    )
-    .where('complete_name', 'ILIKE', `%${query}%`)
-    .orWhere('name', 'ILIKE', `%${query}%`)
-    .orWhere('surname', 'ILIKE', `%${query}%`)
-    .limit(15);
 
   return Promise.all(entities.map(mappingFunction));
 };
--- api/src/db/helpers/entity.js
+++ api/src/db/helpers/entity.js
@@ -188,47 +188,95 @@ async function getAllEntities(params) {
   }));
 }
 
-async function getAllOwnedEntities(type, userId) {
-  const entities = await knex('entities')
-    .select('id', 'type', 'name', 'surname', 'photo_url', 'role')
-    .leftJoin(
-      'entities_name',
-      'entities.id',
-      '=',
-      'entities_name.entity_id',
-    )
-    .leftJoin(
-      'entities_photo',
-      'entities.id',
-      '=',
-      'entities_photo.entity_id',
-    )
-    .leftJoin(
-      'entities_role',
-      'entities.id',
-      '=',
-      'entities_role.entity_id',
-    )
-    .whereNull('deleted_at')
-    .where({ type });
-  const res = await Promise.all(
-    entities.map(async entity => {
-      const role = await getEntityRole(entity.id, userId);
-      return { ...entity, role };
-    }),
+async function getAllOwnedEntities(type, userId, query = '') {
+  // getPersons
+  let entityIds = (
+    await knex('user_entity_role')
+      .select('entity_id')
+      .where({
+        user_id: userId,
+      })
+  ).map(person => ({
+    entity_id: person.entity_id,
+    role: ENTITIES_ROLE_ENUM.ADMIN,
+  }));
+
+  let count = 0;
+  let newEntityIds = [];
+
+  // get all entities owned by persons and sub persons
+  do {
+    entityIds = [...newEntityIds, ...entityIds];
+    entityIds = entityIds.filter(
+      (entity, index) =>
+        entityIds.findIndex(e => e.entity_id === entity.entity_id) ===
+        index,
+    );
+
+    newEntityIds = (
+      await knex('entities_role')
+        .select('entity_id', 'entity_id_admin', 'role')
+        .whereIn(
+          'entity_id_admin',
+          entityIds.map(e => e.entity_id),
+        )
+    ).map(entity => ({
+      ...entity,
+      role: Math.max(
+        entity.role,
+        entityIds.find(e => e.entity_id === entity.entity_id_admin)
+          .role,
+      ),
+    }));
+
+    count++;
+  } while (
+    newEntityIds.some(
+      id => !entityIds.find(e => e.entity_id === id),
+    ) &&
+    count < 5
   );
-  const res2 = res
-    .filter(({ role }) => {
-      return (
-        role === ENTITIES_ROLE_ENUM.ADMIN ||
-        role === ENTITIES_ROLE_ENUM.EDITOR
-      );
-    })
-    .map(e => {
-      const { photo_url: photoUrl, ...otherProps } = e;
-      return { ...otherProps, photoUrl };
-    });
-  return res2;
+
+  const entities = await knex
+    .select('*')
+    .from(
+      knex
+        .select(
+          'id',
+          'type',
+          'name',
+          'surname',
+          knex.raw(
+            "string_agg(entities_all_infos.name || ' ' || entities_all_infos.surname, ' ') AS complete_name",
+          ),
+          'photo_url',
+        )
+        .from('entities_all_infos')
+        .whereNull('deleted_at')
+        .whereIn(
+          'entities_all_infos.id',
+          entityIds.map(e => e.entity_id),
+        )
+
+        .groupBy(
+          'entities_all_infos.id',
+          'entities_all_infos.type',
+          'entities_all_infos.name',
+          'entities_all_infos.surname',
+          'entities_all_infos.photo_url',
+        )
+        .as('res'),
+    )
+    .where('complete_name', 'ILIKE', `%${query || ''}%`)
+    .where({ type });
+
+  return entities.map(entity => ({
+    ...entity,
+    role: entityIds.find(e => e.entity_id === entity.id).role,
+    photo_url: undefined,
+    photoUrl: entity.photo_url,
+    entity_id_admin: undefined,
+  }));
 }
 
 async function getEntitiesName(entityId) {
