2ce1c9fb30da28558d4c9f1945304edad9df8de0
Victor <vicfreve@gmail.com>
2020-12-04 10:22:44 -0500

    WIP

--- api/src/server/utils/ChatBot/states/ScoreSubmissionStates/otherTeamSubmittedAScore.js
+++ api/src/server/utils/ChatBot/states/ScoreSubmissionStates/otherTeamSubmittedAScore.js
@@ -2,14 +2,41 @@ const i18n = require('../../../../../i18n.config');
 const { MESSENGER_QUICK_REPLIES } = require('../../../enums');
 const {
   BASIC_CHATBOT_STATES,
+  SCORE_SUBMISSION_CHATBOT_STATES,
 } = require('../../../../../../../common/enums');
 const { genQuickReply, genText } = require('../../response');
 const State = require('../state');
+const {
+  acceptScoreSuggestion,
+} = require('../../../../../db/queries/entity');
 
 class otherTeamSubmittedAScore extends State {
   async handleEvent(webhookEvent) {
     let nextState;
-    if (this.isNo(webhookEvent)) {
+    const chatbotInfos = this.context.chatbotInfos;
+    if (this.isYes(webhookEvent)) {
+      try {
+        const res = await acceptScoreSuggestion({
+          id: chatbotInfos.suggestionId,
+          submitted_by_roster: chatbotInfos.myRosterId,
+          submitted_by_person: chatbotInfos.playerId,
+        });
+        if (!res) {
+          throw 'error';
+        }
+        nextState =
+          SCORE_SUBMISSION_CHATBOT_STATES.SPIRIT_SUBMISSION_REQUEST_SENT;
+          delete chatbotInfos.suggestionId;
+          delete chatbotInfos.
+      } catch (e) {
+        this.sendMessages(
+          webhookEvent.sender.id,
+          genText(
+            i18n.__('An error happened, do you want to try again?'),
+          ),
+        );
+      }
+    } else if (this.isNo(webhookEvent)) {
       this.sendMessages(
         webhookEvent.sender.id,
         genText(i18n.__('ok_back_to_menu')),
@@ -34,25 +61,22 @@ class otherTeamSubmittedAScore extends State {
 
   async getIntroMessages() {
     const chatbotInfos = this.context.chatbotInfos;
-    const score = this.formatScore(chatbotInfos.score);
+    /*const score = this.formatScore(chatbotInfos.score);
     const submittedBy = chatbotInfos.submittedBy;
     const eventName = chatbotInfos.eventName;
-    console.log({ submittedBy, score, eventName });
-    return {
+    
+  }*/
+  return {
       messages: [
         genText(
           i18n.__(
-            '{{submittedBy}} submitted the score {{score}} for your game of the event {{eventName}}',
-            { submittedBy, score, eventName },
+            '{{submittedBy}} submitted the following score for your game of the event {{eventName}}',
+            { submittedBy, eventName },
           ),
         ),
-        genQuickReply(
-          i18n.__('Do you want to confirm?'),
-          MESSENGER_QUICK_REPLIES.CONFIRMATION,
-        ),
-      ],
+       
+      ], nextState: SCORE_SUBMISSION_CHATBOT_STATES.AWAITING_SCORE_SUBMISSION_CONFIRMATION
     };
-  }
 }
 
 module.exports = otherTeamSubmittedAScore;
