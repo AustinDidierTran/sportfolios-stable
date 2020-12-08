fabec3cf5591c1931e7de2d45406cecab3a96b9e
Victor <vicfreve@gmail.com>
2020-12-08 09:17:53 -0500

    Migration fix

--- api/src/db/migrations/sqls/20201203164536-add-isSub-column-to-player-attendances-table-up.sql
+++ api/src/db/migrations/sqls/20201203164536-add-isSub-column-to-player-attendances-table-up.sql
@@ -1,3 +1,13 @@
-ALTER TABLE game_players_attendance
-  ADD CONSTRAINT game_id_roster_id_player_id_key UNIQUE (game_id, roster_id, player_id),
-  ADD COLUMN is_sub BOOLEAN NOT NULL DEFAULT FALSE;
+DELETE
+FROM GAME_PLAYERS_ATTENDANCE
+WHERE CREATED_AT NOT IN
+    (SELECT MAX(CREATED_AT)
+     FROM GAME_PLAYERS_ATTENDANCE
+     GROUP BY GAME_ID,
+              ROSTER_ID,
+              PLAYER_ID);
+
+
+ALTER TABLE game_players_attendance ADD CONSTRAINT game_id_roster_id_player_id_key UNIQUE (game_id,
+                                                                                           roster_id,
+                                                                                           player_id), ADD COLUMN is_sub BOOLEAN NOT NULL DEFAULT FALSE;
