ce39a17b09ba15408cf4b0b50ccd63ec5a8c0e71
Mederic <med.rochon@gmail.com>
2021-02-09 10:32:57 -0500

    Fix changements pour les migrations

--- api/src/db/migrations/sqls/20210204175527-create-table-for-post-comment-like-up.sql
+++ api/src/db/migrations/sqls/20210204175527-create-table-for-post-comment-like-up.sql
@@ -1,42 +1,25 @@
 /* Replace with your SQL commands */
 CREATE TABLE posts (
   id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-  entity_id UUID NOT NULL,
-  content TEXT NOT NULL,
-  CONSTRAINT fk_entity
-    FOREIGN KEY(entity_id)
-      REFERENCES entities(id)
+  entity_id UUID REFERENCES entities(id) NOT NULL,
+  content TEXT NOT NULL
 );
 
 CREATE TABLE post_image(
-  post_id UUID NOT NULL,
-  image_url VARCHAR (255),
-  CONSTRAINT fk_posts
-    FOREIGN KEY(post_id)
-      REFERENCES posts(id)
+  post_id UUID REFERENCES posts(id) NOT NULL,
+  image_url VARCHAR (255)
+      
 );
 
 CREATE TABLE post_comment (
   id UUID default uuid_generate_v4() PRIMARY KEY,
-  post_id UUID,
+  post_id UUID REFERENCES posts(id) NOT NULL,
   content TEXT NOT NULL,
-  parent_id UUID,
-  CONSTRAINT fk_posts
-    FOREIGN KEY(post_id)
-      REFERENCES posts(id),
-  CONSTRAINT fk_parent
-    FOREIGN KEY(parent_id)
-      REFERENCES post_comment(id)
+  parent_id UUID REFERENCES post_comment(id)
 );
 
 CREATE TABLE post_like (
-  entity_id UUID,
-  post_id UUID,
-  CONSTRAINT fk_entity
-    FOREIGN KEY(entity_id)
-      REFERENCES entities(id),
-  CONSTRAINT fk_posts
-    FOREIGN KEY(post_id)
-      REFERENCES posts(id),
+  entity_id UUID REFERENCES entities(id) NOT NULL,
+  post_id UUID REFERENCES posts(id) NOT NULL,
   PRIMARY KEY (entity_id, post_id)
 );
