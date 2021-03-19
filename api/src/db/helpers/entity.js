3e8770a0499159057c35aff3994bd81cfc7b4998
achille lanctot saumure <achille.lanctots@gmail.com>
2021-03-19 12:00:08 -0400

    modifier une variable pour quelle passe le check

--- api/src/db/helpers/entity.js
+++ api/src/db/helpers/entity.js
@@ -4575,6 +4575,7 @@ async function updateGame(
           .returning('*');
         res.push(r);
       } else {
+        console.log('should be here');
         const [r] = await knex('game_teams')
           .update({
             name: fullName,
