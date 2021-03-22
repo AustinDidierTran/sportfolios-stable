e20f147e6e2dedcd8dc4710769752dc0a1888b70
Mederic <med.rochon@gmail.com>
2021-03-22 11:25:48 -0400

    Enlever tab about et afficher desc dans home

--- controllers/event.js
+++ controllers/event.js
@@ -26,10 +26,6 @@ class EventController {
         value: 'schedule',
         label: 'schedule',
       },
-      {
-        value: 'about',
-        label: 'about',
-      },
     ];
   }
 
@@ -47,7 +43,7 @@ class EventController {
       registrationStart = options.reduce((a, b) => moment(a.startTime) < moment(b.startTime) ? a.startTime : b.startTime, options[0]);
       registrationEnd = options.reduce((a, b) => moment(a.endTime) > moment(b.endTime) ? a.endTime : b.endTime, options[0]);
     }
-
+    console.log(data.description);
     return {
       ...data,
       remainingSpots,
