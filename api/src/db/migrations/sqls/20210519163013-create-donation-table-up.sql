1420a6e8218efaa12fd5d70dac35210ae67b5cd9
Pierre-Etienne <pethebuffon@gmail.com>
2021-05-20 11:20:41 -0400

    changed migration script null by default

--- api/src/db/migrations/sqls/20210519163013-create-donation-table-up.sql
+++ api/src/db/migrations/sqls/20210519163013-create-donation-table-up.sql
@@ -3,7 +3,7 @@ CREATE TABLE donation (
   id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
   amount INTEGER NOT NULL,
   anonyme BOOLEAN NOT NULL DEFAULT FALSE,
-  note VARCHAR(255) NULL,
+  note VARCHAR(255),
   organization_id VARCHAR(255) NOT NULL,
   user_id VARCHAR(255) NOT NULL
 );
