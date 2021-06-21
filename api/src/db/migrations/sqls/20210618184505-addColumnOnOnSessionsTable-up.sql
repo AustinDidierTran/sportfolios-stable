04eb322f7fd6c51939766aeab141a4053ffdbc62
maximep12 <49524374+maximep12@users.noreply.github.com>
2021-06-21 09:14:53 -0400

    Update api/src/db/migrations/sqls/20210618184505-addColumnOnOnSessionsTable-up.sql
    Co-authored-by: Julien Bernat <57548160+julienbernat@users.noreply.github.com>

--- api/src/db/migrations/sqls/20210618184505-addColumnOnOnSessionsTable-up.sql
+++ api/src/db/migrations/sqls/20210618184505-addColumnOnOnSessionsTable-up.sql
@@ -1,4 +1,3 @@
 ALTER TABLE evaluations ALTER COLUMN session_id DROP NOT NULL;
 ALTER TABLE evaluations
- ADD COLUMN game_id uuid,
- ADD FOREIGN KEY (game_id) REFERENCES games(id);
+ ADD COLUMN game_id uuid REFERENCES games(id);
