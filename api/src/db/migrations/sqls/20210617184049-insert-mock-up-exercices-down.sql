9dce38131b65adef48d84dccd09a2cd12b89c7c1
julienbernat <57548160+julienbernat@users.noreply.github.com>
2021-06-21 14:19:19 -0400

    Fix migration

--- api/src/db/migrations/sqls/20210617184049-insert-mock-up-exercices-down.sql
+++ api/src/db/migrations/sqls/20210617184049-insert-mock-up-exercices-down.sql
@@ -1,1 +1,6 @@
 /* Replace with your SQL commands */
+ALTER TABLE sessions ADD COLUMN entity_id UUID REFERENCES entities(id);
+
+ALTER TABLE evaluations
+  ADD COLUMN event_id UUID,
+  DROP COLUMN session_id;
