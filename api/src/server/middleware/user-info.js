a15ddfa4e3599a1204eb36da41d7131d74fc5392
Austin-Didier Tran <tran.austin.didier@gmail.com>
2021-09-05 03:06:58 -0400

    Added logs for debug

--- api/src/server/middleware/user-info.js
+++ api/src/server/middleware/user-info.js
@@ -2,19 +2,24 @@ import knex from '../../db/connection.js';
 import { ERROR_ENUM } from '../../../../common/errors/index.js';
 
 export default async (ctx, next) => {
+  console.log(1);
   const token = ctx.headers['authorization'];
+  console.log(2);
 
   if (!token || token === 'null') {
     return next();
   }
+  console.log(3);
 
   const [user] = await knex('user_token')
     .select(['user_id', 'expires_at'])
     .where({ token_id: token });
+  console.log(4);
 
   if (!user) {
     return next();
   }
+  console.log(5);
 
   const { user_id, expires_at } = user;
 
@@ -22,6 +27,7 @@ export default async (ctx, next) => {
     return next();
   }
 
+  console.log(7);
   if (expires_at < new Date()) {
     if (!ctx.body) {
       ctx.body = {};
@@ -29,24 +35,31 @@ export default async (ctx, next) => {
     ctx.body.userInfo = { error: ERROR_ENUM.TOKEN_EXPIRED };
     return next();
   }
+  console.log(8);
 
   const userInfo = {
     id: user_id,
   };
+  console.log(9);
 
   const [{ app_role } = {}] = await knex('user_app_role')
     .select('app_role')
     .where({ user_id });
+  console.log(10);
 
   if (app_role) {
     userInfo.appRole = app_role;
   }
+  console.log(11);
 
   if (ctx.body) {
+    console.log(12);
     ctx.body.userInfo = userInfo;
   } else {
+    console.log(13);
     ctx.body = { userInfo };
   }
+  console.log(14);
 
   return next();
 };
