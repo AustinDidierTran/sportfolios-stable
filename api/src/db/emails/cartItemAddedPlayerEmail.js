685dcb1061a8c2c0e0effbb75774bb3e521f4845
Austin-Didier Tran <tran.austin.didier@gmail.com>
2021-09-07 13:40:11 -0400

    Removed logs

--- api/src/db/emails/cartItemAddedPlayerEmail.js
+++ api/src/db/emails/cartItemAddedPlayerEmail.js
@@ -7,31 +7,24 @@ import { fileURLToPath } from 'url';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default async function CartItemAddedPlayerEmail(infos) {
-  console.log('d', 1);
   const { teamName, eventName, locale, buttonLink } = infos;
-
-  console.log(infos);
 
   const text = i18n.__(
     { phrase: 'emails.cart_item_added_player_text', locale },
     teamName,
     eventName,
   );
-  console.log('d', 2);
   const buttonText = i18n.__({
     phrase: 'emails.cart_item_added_player_button',
     locale,
   });
-  console.log('d', 3);
   const subject = i18n.__({
     phrase: 'emails.cart_item_added_player_subject',
     locale,
   });
-  console.log('d', 4);
   const html = await ejs.renderFile(
     __dirname + '/templates/textAndButton.ejs',
     { buttonLink, text, buttonText },
   );
-  console.log('d', 5);
   return { html, subject };
 }
