426efd8687ac0844081d3f29b8bb940fc2da4445
ClaudeGP <31514003+ClaudeGP@users.noreply.github.com>
2021-09-14 16:37:38 -0400

    cleanup import

--- api/src/server/routes/private/event.js
+++ api/src/server/routes/private/event.js
@@ -1,7 +1,6 @@
 import Router from 'koa-router';
 import * as service from '../../service/event.js';
-import { STATUS_ENUM } from '../../../../../common/enums/index.js';
-import { ERROR_ENUM, errors } from '../../../../../common/errors/index.js';
+import { ERROR_ENUM } from '../../../../../common/errors/index.js';
 
 const router = new Router();
 const BASE_URL = '/api/event';
