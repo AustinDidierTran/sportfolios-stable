cd95a443f272ac29a0d35e3294d858c253ac5c3f
Austin-Didier Tran <tran.austin.didier@gmail.com>
2021-09-17 22:31:47 -0400

    Wcs 43/spirit submission (#982)
    * Added a route that has more details for shop items
    
    * There is now a route to submit the spirit

--- api/src/server/service/game.js
+++ api/src/server/service/game.js
@@ -4,6 +4,51 @@ import { isAllowed } from './entity.js';
 
 import * as queries from '../../db/queries/game.js';
 
+export const submitSpiritScore = async (body, userId) => {
+  const {
+    submittedByPerson,
+    submittedByRoster,
+    submittedForRoster,
+    gameId,
+    spiritScore,
+    comment,
+  } = body;
+
+  if (
+    !(await isAllowed(
+      submittedByPerson,
+      userId,
+      ENTITIES_ROLE_ENUM.EDITOR,
+    ))
+  ) {
+    throw new Error(ERROR_ENUM.ACCESS_DENIED);
+  }
+
+  if (
+    queries.isSpiritAlreadySubmmited(
+      gameId,
+      submittedByRoster,
+      submittedForRoster,
+    )
+  )
+    // Add spirit to score submission
+    await queries.submitSpiritScore({
+      submittedByPerson,
+      submittedByRoster,
+      submittedForRoster,
+      gameId,
+      spiritScore,
+      comment,
+    });
+
+  // As of now, set the score of the game to the said spirit
+  await queries.updateGameSpirit({
+    submittedForRoster,
+    gameId,
+    spiritScore,
+  });
+};
+
 export const updateGameScore = async (body, userId) => {
   const { eventId, gameId, rosters } = body;
 
