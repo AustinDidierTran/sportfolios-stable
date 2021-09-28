48cc46b94397905ed0e50d3efc8ea91729df3287
Austin-Didier Tran <tran.austin.didier@gmail.com>
2021-09-28 15:23:08 -0400

    Fixed import

--- api/src/server/routes/private/entity.js
+++ api/src/server/routes/private/entity.js
@@ -1,6 +1,6 @@
 import Router from 'koa-router';
 import * as service from '../../service/entity-deprecate.js';
-import * as organizationService from '../../service/organization';
+import * as organizationService from '../../service/organization.js';
 import { STATUS_ENUM } from '../../../../../common/enums/index.js';
 import {
   ERROR_ENUM,
