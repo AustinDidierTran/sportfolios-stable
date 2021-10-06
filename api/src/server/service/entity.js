61d6bffeadbbfe366b9a3e45e4b1e5cc37c724ff
PC-OLI\Olivier <bellavance.o@gmail.com>
2021-10-06 18:03:08 -0400

    remove async

--- api/src/server/service/entity.js
+++ api/src/server/service/entity.js
@@ -12,31 +12,29 @@ export const getForYouPagePosts = async () => {
   const events = await eventQueries.getEventVerified();
   const merch = await shopQueries.getActiveStoreItemsAllInfos();
 
-  const fullEvents = await Promise.all(
-    events.map(async event => {
+  const fullEvents = events.map(event => {
 
-      return {
-        type: GLOBAL_ENUM.EVENT,
-        cardType: CARD_TYPE_ENUM.EVENT,
-        eventId: event.id,
-        photoUrl: event.photo_url,
-        startDate: event.start_date,
-        endDate: event.end_date,
-        quickDescription: event.quick_description,
-        description: event.description,
-        location: event.location,
-        name: event.name,
-        createdAt: event.created_at,
-        creator: {
-          id: event.creatorEntities.id,
-          type: event.creatorEntities.type,
-          name: event.creatorEntities.entitiesGeneralInfos.name,
-          surname: event.creatorEntities.entitiesGeneralInfos.surname,
-          photoUrl: event.creatorEntities.entitiesGeneralInfos.photo_url,
-        },
-      };
-    }),
-  );
+    return {
+      type: GLOBAL_ENUM.EVENT,
+      cardType: CARD_TYPE_ENUM.EVENT,
+      eventId: event.id,
+      photoUrl: event.photo_url,
+      startDate: event.start_date,
+      endDate: event.end_date,
+      quickDescription: event.quick_description,
+      description: event.description,
+      location: event.location,
+      name: event.name,
+      createdAt: event.created_at,
+      creator: {
+        id: event.creatorEntities.id,
+        type: event.creatorEntities.type,
+        name: event.creatorEntities.entitiesGeneralInfos.name,
+        surname: event.creatorEntities.entitiesGeneralInfos.surname,
+        photoUrl: event.creatorEntities.entitiesGeneralInfos.photo_url,
+      },
+    };
+  });
   const fullMerch = merch
     .filter(m => m.metadata.type === CART_ITEM.EVENT)
     .map(item => ({
