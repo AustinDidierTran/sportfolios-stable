044667aa495edd691ef3fe9e0c1a03b8b6d7c7eb
Austin-Didier Tran <tran.austin.didier@gmail.com>
2021-10-07 16:09:00 -0400

    Another log...

--- api/src/server/service/entity-deprecate.js
+++ api/src/server/service/entity-deprecate.js
@@ -595,7 +595,7 @@ async function getPossibleSubmissionerInfos(gameId, teams, userId) {
       if (!admins) {
         return;
       }
-      console.log(5);
+      console.log(5, t.rosterId);
       const myTeam = teamsList.find(
         team => team.rosterId === t.rosterId,
       );
