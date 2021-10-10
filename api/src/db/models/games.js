ab2f688459a2907f6066ca27a6c3afba87c1a000
obellavance <bellavance.o@gmail.com>
2021-10-09 21:02:55 -0400

    refactor fonction createEvent + type Game (#998)

---
+++ api/src/db/models/games.js
@@ -0,0 +1,30 @@
+import { Model } from 'objection';
+import { events } from './events.js';
+import { entities } from './entities.js';
+
+export class games extends Model {
+  static get tableName() {
+    return 'games';
+  }
+
+  static get relationMappings() {
+    return {
+      event: {
+        relation: Model.HasOneRelation,
+        modelClass: events,
+        join: {
+          from: 'games.event_id',
+          to: 'events.id'
+        }
+      },
+      entity: {
+        relation: Model.HasOneRelation,
+        modelClass: entities,
+        join: {
+          from: 'games.id',
+          to: 'entities.id'
+        }
+      },
+    };
+  }
+}
