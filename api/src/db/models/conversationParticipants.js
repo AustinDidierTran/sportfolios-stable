f9a11a24d7f7583154a5e167f12629b75660c66a
PC-OLI\Olivier <bellavance.o@gmail.com>
2021-11-14 20:21:11 -0500

    only return conversation with the exact match of participant

--- api/src/db/models/conversationParticipants.js
+++ api/src/db/models/conversationParticipants.js
@@ -38,6 +38,14 @@ export class conversationParticipants extends Model {
           to: 'entities_general_infos.entity_id',
         },
       },
+      conversationsParticipant: {
+        relation: Model.HasManyRelation,
+        modelClass: conversationParticipants,
+        join: {
+          from: 'conversation_participants.conversation_id',
+          to: 'conversation_participants.conversation_id',
+        },
+      },
     };
   }
 }
