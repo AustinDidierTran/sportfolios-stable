b50b1f33b7deb194b9866cfe8efbdb1912e14e81
PC-OLI\Olivier <bellavance.o@gmail.com>
2021-12-05 12:57:37 -0500

    Correctif eslint

--- api/src/server/routes/private/messaging.js
+++ api/src/server/routes/private/messaging.js
@@ -61,7 +61,6 @@ router.post(`${BASE_URL}/conversation`, async ctx => {
   ctx.body = { data: conversationId };
 });
 
-export default router;
 /** PUT */
 
 router.put(`${BASE_URL}/addParticipants`, async ctx => {
